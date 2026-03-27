0cd249d5b3428753de767b4917fb3e585e4668fe
Carlos Sandoval <samcdvg17@gmail.com>
2026-03-27 16:52:10 -0600

    fix: clear axios global baseURL to avoid /api/api double prefix

--- backend-laravel/resources/js/bootstrap.js
+++ backend-laravel/resources/js/bootstrap.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = '/api';
+window.axios.defaults.baseURL = '';
 window.axios.defaults.withCredentials = true;
 
 // Configurar token al iniciar
